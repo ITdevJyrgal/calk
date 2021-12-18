c0c07e809d1d9cc6ed5e10753d2042db5e3ba5f8
jyrgalbek04 <jyldyzbekuulujyrgalbek@gmail.com>
2021-12-18 14:38:47 +0600

    index

---
+++ js/index.js
@@ -0,0 +1,51 @@
+// let btn = document.querySelector(".click")
+//
+// const firstInput = document.querySelector(".first-input")
+// const secondInput = document.querySelector(".second-input")
+// const result = document.querySelector(".result")
+// result.value =  + firstInput.value +  +secondInput.value
+//
+// btn.addEventListener("click", () => {
+//     btn.classList.add("bg-danger")
+//     console.log(typeof firstInput.value)
+//     result.value = + firstInput.value + +  secondInput.value
+// })
+
+// const boxes = document.querySelectorAll(".box")
+//
+// ForEach()
+// boxes.forEach( box => {
+//     return box.addEventListener("click", () => {
+//         box.classList.add("bg-dark")
+//
+//     })
+// })
+//
+//
+//
+//
+// let btn = document.getElementsByClassName("click")
+//
+// console.log(btn)
+//
+
+
+// const btn = document.getElementById("first")
+// console.log(btn)
+//
+
+
+// const btn = document.getElementsByName('button')
+//
+// console.log(btn)
+//
+//
+// let btn = document.querySelector(".btn--item")
+// btn.addEventListener("click", () => {
+//     document.querySelector(".box").style.display = "block"
+// })
+//
+// let closeBtn = document.querySelector(".btn2")
+// closeBtn.addEventListener("click", () =>  {
+//     document.querySelector(".box").style.display = "none"
+// })
